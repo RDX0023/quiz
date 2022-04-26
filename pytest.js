6fdde5b91117ef661fddb0882445f0ffb296fa62
RDX0023 <68907503+RDX0023@users.noreply.github.com>
2022-04-26 14:25:27 +0530

    Add files via upload

---
+++ pytest.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is the maximum possible length of an identifier?",
+    answer: "None",
+    options: [
+      "16",
+      "32",
+      "64",
+      "None"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who developed the Python language?",
+    answer: "Guido van Rossum",
+    options: [
+      "Zim Den",
+      "Guido van Rossum",
+      "Niene Stom",
+      "Wick van Rossum"
+	  ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following statements is correct regarding the object-oriented programming concept in Python?",
+    answer: "Objects are real-world entities while classes are not real",
+    options: [
+      "Classes are real-world entities while objects are not real",
+      "Objects are real-world entities while classes are not real",
+      "Both objects and classes are real-world entities",
+      "All of the above"
+	 ]
+  },
+    {
+    numb: 4,
+    question: "What is the method inside the class in python language?",
+    answer: "Function",
+    options: [
+      "Object",
+      "Function",
+      "Attribute",
+      "Argument"
+    ]
+  },
+    {
+    numb: 5,
+    question: " Which of the following declarations is incorrect?",
+    answer: "None of these",
+    options: [
+    "_x = 2",
+    "__x = 3",
+    "__xyz__ = 5",
+    "None of these"
+    ]
+  },
+
+   
+];
